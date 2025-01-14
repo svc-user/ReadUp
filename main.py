7eeab8ffcd6ffc0e7b16c38d77c7a4d4c01bc350
Nikolaj <2525536+svc-user@users.noreply.github.com>
2025-01-14 10:30:49 +0100

    add requirements.txt file. Remove OpenAI token from source

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from openai import OpenAI
 from selenium import webdriver
-import pathlib
 import pyaudio
+import os, sys, pathlib, hashlib
 
-API_KEY = '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'
-
-
+API_KEY = os.environ["OPENAI_KEY"]
+PROJECT_ID = os.environ["PROJECT_ID"] 
+BASE_DIR = pathlib.Path(__file__).parent / "articles"
 
 def getWebsite(site_url):
     options = webdriver.FirefoxOptions()
-    # options.add_argument("-headless")
+    options.add_argument("-headless")
     driver = webdriver.Firefox(keep_alive=False, options=options)
     driver.get(site_url)
     markup = driver.page_source
@@ -17,24 +17,33 @@ def getWebsite(site_url):
     return markup
 
 
-def getArticle(markup):
-    openai = OpenAI(api_key=API_KEY)
+def getArticle(article_hash, markup):
+    if (BASE_DIR / article_hash).exists():
+        with open(BASE_DIR / article_hash, "rt") as f:
+            cached = f.read()
+            return cached
+    
+    openai = OpenAI(api_key=API_KEY, project=PROJECT_ID)
     completion = openai.chat.completions.create(
         model="gpt-4o-mini", 
         stream=False,
         messages=[ {
                 "role": "user", 
-                "content": "Given the following website markup, return only the text of the site that is an article, blog post or similar. The ouput MUST be formatted as human readable text with no kind of markup. If there are images include them by mentioning that there's an image and the images alt text - if any.\n-------\n"
+                "content": "Given the following website markup, return only the text of the site that is an article, blog post or similar. The ouput MUST be formatted as human readable text with no kind of markup. It's important to extract the article as verbatim as possible. If there are images include them by mentioning that there's an image and the images alt text - if any.\n-------\n"
                     + markup
             }
         ]
     )
-    return completion.choices[0].message.content
+
+    text = completion.choices[0].message.content
+    with open(BASE_DIR / article_hash, "wt") as f:
+        f.write(text)
+    
+    return text
 
 
-def tts(text):
-
-    openai = OpenAI(api_key=API_KEY)
+def tts(article_hash, text):
+    openai = OpenAI(api_key=API_KEY, project=PROJECT_ID)
     player_stream = pyaudio.PyAudio().open(format=pyaudio.paInt16, channels=1, rate=24000, output=True)
 
     in_text = text
@@ -44,29 +53,38 @@ def tts(text):
         chunks.append(in_text[:rspc])
         in_text = in_text[rspc+1:]
     else:
-        chunks = [in_text]
+        chunks.append(in_text)
 
     chunks = [(i, c) for i, c in enumerate(chunks)]
     for i, chunk in chunks:
-        out_path = pathlib.Path(__file__).parent / ("tmp_" + str(i) + ".mp3")
+        out_path = BASE_DIR / (article_hash + "_" + str(i) + ".pcm")
+        
+        with open(out_path, 'wb') as out_file:
+            with openai.audio.with_streaming_response.speech.create(
+                model="tts-1", 
+                voice="onyx",
+                input=chunk,
+                response_format="pcm"
+            ) as tts_stream:
+                for audio_chunk in tts_stream.iter_bytes(chunk_size=1024):
+                    player_stream.write(audio_chunk)
+                    out_file.write(audio_chunk)
 
-        with openai.audio.with_streaming_response.speech.create(
-            model="tts-1", 
-            voice="alloy",
-            input=chunk,
-            response_format="pcm"
-        ) as tts_stream:
-            # tts_stream.stream_to_file(out_path)
-            for audio_chunk in tts_stream.iter_bytes(chunk_size=1024):
-                player_stream.write(audio_chunk)
 
 
 
 if __name__ == '__main__':
-    print("Fetching page..")
-    # page_markup = getWebsite("https://jsrn.net/2024/11/14/choosing-a-todo-app.html")
-    page_markup = getWebsite("https://blog.djhaskin.com/blog/why-i-chose-common-lisp/")
+    url = sys.argv[1]
+
+    if not pathlib.Path(BASE_DIR).exists():
+        os.mkdir(BASE_DIR)
+
+    print("Fetching page at %s.." % url)
+    page_markup = getWebsite(url)
+    article_hash = hashlib.sha256(page_markup.encode()).hexdigest()
+    print("Article hash is: %s" % article_hash)
+
     print("Extracting text...")
-    article = getArticle(page_markup)
+    article = getArticle(article_hash, page_markup)
     print("Converting to speech...")
-    tts(article)
+    tts(article_hash, article)
